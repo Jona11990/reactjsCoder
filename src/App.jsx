74c57453aaab642de2f51dbb38bb9d06820c9e14
Jonathan Mello <jony_river89@hotmail.com>
2023-09-27 22:43:20 -0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+function App() {
+  return (
+<>
+    <header>
+      <h1>Sublimaciones Lorena</h1>
+    </header>
+
+    <main>
+      <h2>Home</h2>
+    </main>
+
+    <footer>
+      <p>Copyrigth 2023 - Jonathan Mello</p>
+    </footer>
+
+
+</>
+
+  )
+}
+
+export default App
